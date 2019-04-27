cdca7dade3e88af0932fb8dbf69c0066bafd5d8b
KiboOst <KiboOst@users.noreply.github.com>
2019-04-27 12:45:24 +0200

    remove debug

--- desktop/js/jeexplorer.js
+++ desktop/js/jeexplorer.js
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
-console.log('language:' + jeexplorerLang)
+
 $().ready(function() {
   var options = {
       url  : 'plugins/jeexplorer/3rdparty/elfinder/php/connector.minimal.php',
